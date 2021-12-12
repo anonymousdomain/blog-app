abc5c2b9ca67e2efe6b993ca8d051cddf8b1a816
anonymousdomain <domainanonymous1@gmail.com>
2021-12-12 17:17:12 +0300

    base templates and home templates dumy data used for testing the templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+posts=[
+    {
+        'title':'blog posts',
+        'author':'Dawit Mekonnen',
+        'date_posted':'sep 2 2021',
+        'content':'programing languge'
+    },
+     {
+        'title':'tplf is terrerist',
+        'author':'nahom bezin ',
+        'date_posted':'sep 2 2021',
+        'content':'as we all know that tplf started a war in november 3 20201 and still now '
+    }
+]
 def home(request):
-    return render(request,'blog/home.html')
+    context={'posts':posts}
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'title'})
