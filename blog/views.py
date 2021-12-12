eb72912b226bdd89e5251866750e1fd1f59f2ed0
anonymousdomain <domainanonymous1@gmail.com>
2021-12-12 18:48:11 +0300

    rendering post contents

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 # Create your views here.
-posts=[
-    {
-        'title':'blog posts',
-        'author':'Dawit Mekonnen',
-        'date_posted':'sep 2 2021',
-        'content':'programing languge'
-    },
-     {
-        'title':'tplf is terrerist',
-        'author':'nahom bezin ',
-        'date_posted':'sep 2 2021',
-        'content':'as we all know that tplf started a war in november 3 20201 and still now '
-    }
-]
 def home(request):
-    context={'posts':posts}
+    context={
+        'posts':Post.objects.all()
+    }
     return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html',{'title':'title'})
+    return render(request,'blog/about.html')
