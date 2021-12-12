8ce2f120e69fe40552a63f2106d2324ca7f9f9d4
anonymousdomain <domainanonymous1@gmail.com>
2021-12-12 15:09:12 +0300

    getting started with template

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>home</h1>')
+    return render(request,'blog/home.html')
 
 def about(request):
-    return HttpResponse('<h1>about</h1>')
+    return render(request,'blog/about.html')
