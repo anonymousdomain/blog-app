f329a718ef419ea21c59e417a56d4e0c499662e3
anonymousdomain <domainanonymous1@gmail.com>
2021-12-14 21:01:46 +0300

    create post

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (ListView,DetailView
+                                  ,CreateView)
 from .models import Post
 # Create your views here.
-def home(request):
-    context={
-        'posts':Post.objects.all()
-    }
-    return render(request,'blog/home.html',context)
 
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model=Post
+    #modify the template pattern that list view class excpect
+    template_name='blog/home.html'
+    context_object_name='posts'
+    #post ordered by latest time post
+    ordering=['-posted_date']
+class PostDetailView(DetailView):
+    model=Post
+    
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=Post
+    fields=['title','content']
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+    
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request, 'blog/about.html')
