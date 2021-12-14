e2e42b2e25a224cf7e0bf6c1dfa225bfe19e9ba6
anonymousdomain <domainanonymous1@gmail.com>
2021-12-14 21:16:13 +0300

    update post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (ListView,DetailView
-                                  ,CreateView)
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        UserPassesTestMixin)
+from django.views.generic import (ListView,
+                                  DetailView
+                                  ,CreateView
+                                  ,UpdateView)
 from .models import Post
 # Create your views here.
 
@@ -29,6 +32,18 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form):
         form.instance.author=self.request.user
         return super().form_valid(form)
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post
+    fields=['title','content']
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user==post.author:
+            return True
+        else:
+            return False
     
 def about(request):
     return render(request, 'blog/about.html')
